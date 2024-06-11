6b28d7ff52bf3005d034c9be65541bfa3e12b102
Thomas <thomas.marie.duval@outlook.fr>
2024-06-11 17:06:45 +0200

    Edit ping to have spec of a slashcommand

--- Commande/ping.js
+++ Commande/ping.js
@@ -2,9 +2,12 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "ping",
+    description: "Affiche la latence",
+    permission: "Aucune",
+    dm: true,
 
     async run(bot, message){
-
-        await message.reply(`Ping : \`${bot.ws.ping}\``);
+        await message.reply("Ping : " + bot.ws.ping)
     }
+
 }
