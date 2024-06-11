6e351fd409b85ddf6ab00ef34917291e381edba4
Thomas <thomas.marie.duval@outlook.fr>
2024-06-11 15:15:27 +0200

    adding loadCommands.js

---
+++ Loader/loadCommands.js
@@ -0,0 +1,19 @@
+// noinspection ES6MissingAwait
+
+const fs = require("fs");
+
+module.exports = async bot => {
+    //let FilesInCommande = fs.readdirSync("./Commande");
+
+    fs.readdirSync("./Commande").filter(f => f.endsWith(".js")).forEach(async file => {
+
+        let command = require(`../Commande/${file}`); //Récupère la commande
+
+        if(!command.name || typeof command.name !== "string"){ //Vérification du nom de la commande
+            throw new TypeError("La commande " + file + " n'a pas de nom !");
+        }
+
+        bot.commands.set(command.name, command); //Crée une commande avec le nom de la commande rattaché au fichier command ("testcommande.js" par exemple)
+        console.log("Commande " + file + " chargée avec succès !");
+    })
+}
