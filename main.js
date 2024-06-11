6fad7aa48c69ae91798818e13c03a94f2459950d
Thomas <thomas.marie.duval@outlook.fr>
2024-06-11 15:58:46 +0200

    Update Main

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
 const Discord = require("discord.js"); //import du package discord.js
 const intents = new Discord.IntentsBitField(3276799);
+
 const bot = new Discord.Client({intents}); //Création du bot
+//Chargement de nos loader
 const loadCommands = require("./Loader/loadCommands");
+const loadEvents = require("./Loader/loadEvents");
+
 const config= require("./config"); //import de config.js
 
 bot.commands = new Discord.Collection();
 
 bot.login(config.token); //récupération du token dans variable config
+//Executions de nos loader avec notre bot en param
 loadCommands(bot);
+loadEvents(bot);
 
-bot.on("messageCreate", async message => {
-    console.log("Un message a été envoyé voici sont contenu : " + message.content);
-    if(message.content == "!ping"){
-        console.log("test2")
-        return bot.commands.get("ping").run(bot, message);
-    }
-})
-
-bot.on("ready", () => { //fonction assync quand le bot est allumé
-    console.log(bot.user.tag + " est bien en ligne !"); //récupérat!ion du tag du bot
-});
 
