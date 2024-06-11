04e035bda33c702618667ebdb3ff6446093afd8e
Thomas <thomas.marie.duval@outlook.fr>
2024-06-11 15:58:39 +0200

    Update loadEvents.js

---
+++ Loader/loadEvents.js
@@ -0,0 +1,15 @@
+// noinspection ES6MissingAwait
+
+const fs = require("fs");
+
+module.exports = async bot => {
+    //let FilesInCommande = fs.readdirSync("./Commande");
+
+    fs.readdirSync("./Event").filter(f => f.endsWith(".js")).forEach(async file => {
+
+        let event = require(`../Event/${file}`); //Récupère la commande
+        bot.on(file.split(".js").join(""), event.bind(null, bot));
+
+        console.log("Evènement " + file + " chargé avec succès !");
+    })
+}
